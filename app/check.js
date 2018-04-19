9d7afe0ca470e535fd618fb92de3496c7ed57d9b
Give money to charity <henrysavit@gmail.com>
2018-04-19 10:31:43 -0400

    here is le code

---
+++ app/check.js
@@ -0,0 +1,124 @@
+'use strict';
+const isEmpty = str => str.replace(/^\s+|\s+$/gm, '').length == 0;
+const hasLength = (input) => input.length !== 0;
+const atLeast8 = input => input.length >= 8;
+const atMost64 = input => input.length <= 64;
+const isASCII = input => /^[\x00-\x7F]*$/.test(input);
+
+const getFullWord = (index, haystack) => {
+	const getRightSideWord = (index, haystack) => {
+		let full_word = '';
+		const max_word_length_index = index + 100;
+		for (index; index < max_word_length_index; index++) {
+			let char = haystack.charAt(index);
+			if (isEmpty(char)) {
+				return full_word;
+			} else {
+				full_word = full_word + char;
+			}
+		}
+		return full_word;
+	};
+
+	const getLeftSideWord = (index, haystack) => {
+		let full_word = '';
+		let i = index - 1;
+		const max_word_length_index = index - 100;
+		for (i; index > max_word_length_index; i--) {
+			let char = haystack.charAt(i);
+			if (isEmpty(char)) {
+				return full_word;
+			} else {
+				full_word = char + full_word;
+			}
+		}
+		return full_word;
+	};
+
+	return getLeftSideWord(index, haystack) + getRightSideWord(index, haystack);
+};
+
+const isUnique = (needle, haystack) => {
+	if (haystack.includes(needle)) {
+		//basic includes doesnt protect against 99999frbnzy (actual is 999999999frbnzy)
+		//check if needle is a substring of a bigger word
+
+		var index = haystack.indexOf(needle);
+		while (index !== -1) {
+
+			//get word
+			let full_word = getFullWord(index, haystack);
+
+			if (needle == full_word) {
+				return false;
+			}
+
+			//check all occurrences that this may be a substring of
+			//goes to the next occurrence
+			index = haystack.indexOf(needle, index + 1);
+		}
+	}
+
+	return true;
+};
+
+function check(passwords) {
+	var button = document.getElementById('button');
+
+	button.onclick = function() {
+		var password = document.getElementById('password_field').value;
+
+		var pw = document.getElementById('a');
+		pw.innerHTML = `Password inputted: <|${password}|>`;
+
+		if (hasLength(password)) {
+			if (atLeast8(password)) {
+				if (atMost64(password)) {
+					if (isASCII(password)) {
+						if (
+							isUnique(password, passwords)
+						) {
+							alert('Nice Password');
+						} else {
+							alert(
+								'Password is a common password. Use a stronger password'
+							);
+						}
+					} else {
+						alert(
+							'Password must be only ASCII characters and spaces'
+						);
+					}
+				} else {
+					alert('Password exceeds maximum length of 64 characters');
+				}
+			} else {
+				alert('Password must have a minimum length of 8 characters');
+			}
+		}
+	};
+}
+
+async function fetchPws() {
+	const response = await fetch('http://localhost:3000/passwords');
+	const passwords = await response.text();
+	// TODO: parse and store passwords.
+
+	if (passwords) {
+		check(passwords);
+	}
+	else {
+		alert('Could not load passwords. Cannot proceed at this time. :( ');
+	}
+}
+
+fetchPws();
+
+var exports = (module.exports = {});
+exports.hasLength = hasLength;
+exports.atLeast8 = atLeast8;
+exports.isUnique = isUnique;
+exports.isEmpty = isEmpty;
+exports.atMost64 = atMost64;
+exports.isASCII = isASCII;
+exports.getFullWord = getFullWord;
---
+++ test/test.js
@@ -0,0 +1,92 @@
+const check = require('../app/check.js');
+const assert = require('assert');
+
+const sample_words = "99ariA9r 99aria99 99accord 99a99s99 999xxx 999ooo999 999gfx 999con 999az999 99999z 99999d 999999z 999999o 999999d 999999999frbnzy 9999996 99999934 999995 999988445 9999666 99992222 9999122097 999876 9997 99966 999641146999 999555111";
+
+describe('Testing the length function', function() {
+	it('Should pass if given a string of some length', function() {
+		assert.equal(true, check.hasLength('12345678'));
+	});
+});
+
+describe('Testing the length function', function() {
+	it('Should fail if given a string of no length', function() {
+		assert.equal(false, check.hasLength(''));
+	});
+});
+
+describe('Testing the at least 8 chars function', function() {
+	it('Should pass if given a string of 8 characters', function() {
+		assert.equal(true, check.atLeast8('12345678'));
+	});
+});
+
+describe('Testing the at least 8 chars function', function() {
+	it('Should fail if given a string of 4 characters', function() {
+		assert.equal(false, check.atLeast8('1234'));
+	});
+});
+
+describe('Testing the at most 64 chars function', function() {
+	it('Should pass if given a string of 64 characters', function() {
+		assert.equal(true, check.atMost64('8888888888888888888888888888888888888888888888888888888888888888'));
+	});
+});
+
+describe('Testing the at most 64 chars function', function() {
+	it('Should fail if given a string of 1000 characters', function() {
+		assert.equal(false, check.atMost64('888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888'));
+	});
+});
+
+describe('Testing the ASCII function', function() {
+	it('Should pass if given a string that contains ascii characters', function() {
+assert.equal(true, check.isASCII('kjahw1234586791353134091+_"L></.,~fbkafhi34fhaifgakef lakfalwkfhalO&*#$@#&*$O@$&P^%@Q,ahA a,'));
+	});
+});
+
+describe('Testing the ASCII function', function() {
+	it('Should fail if given a string that contains non-ascii characters', function() {
+		assert.equal(false, check.isASCII('ابتابتابتابتaerfaerfa'));
+	});
+});
+
+describe('Testing the getFullWordFunction', function() {
+	it('Given the starting index of a string that is included in a bigger string, return that bigger string', function() {
+assert.equal('abcdef123456ghijk', check.getFullWord(25, 'wrtglwnrg;lwr abcdef123456ghijk ;wlrtgn;wrlgn w;tlgk;wrtg wr;tlgk'));
+	});
+});
+
+describe('Testing the isUnique function', function() {
+	it('Should fail if the string is found in a list of common strings', function() {
+		assert.equal(false, check.isUnique('999999d', sample_words, '99aria99', '999555111'));
+	});
+});
+
+describe('Testing the isUnique function', function() {
+	it('Should pass if the string is not in a list of common strings', function() {
+		assert.equal(true, check.isUnique('aerfaerfaerfa', sample_words));
+	});
+});
+
+describe('Testing the isUnique function', function() {
+	it('Should pass if the string is a unique string but also a substring of a bigger string in a list of common strings', function() {
+		assert.equal(true, check.isUnique('999999frbnzy', sample_words));
+	});
+});
+
+describe('Testing the isUnique function', function() {
+	it('Should pass if the word contains the same characters as a different word but different capitalization', function() {
+		assert.equal(false, check.isUnique('99ariA9r', sample_words));
+	});
+});
+
+describe('Testing all scenarios', function() {
+	it('Should pass if string fulfills all the requirements', function() {
+
+        const word = 'kjrfnlknflkj';
+        const result = check.atLeast8(word) && check.atMost64(word) && check.isASCII(word) && check.isUnique(word, sample_words); 
+
+		assert.equal(true, result);
+	});
+});
